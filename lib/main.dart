5a5a3ac4fba2604b9b030e6b352345bd9e3b459c
adarsh99988 <adarshtiwari.0945@gmail.com>
2025-07-21 12:51:31 +0530

    Completed.

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+// import 'package:auth_demo/home_screen.dart';
+// import 'package:auth_demo/login_screen.dart';
+// import 'package:auth_demo/signup_screen.dart';
+// import 'package:auth_demo/wrapper_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_connect/signup_screen.dart';
+import 'package:flutter_firebase_connect/wrapper_screen.dart';
+
+import 'home_screen.dart';
+import 'login_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Auth UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: false),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => const WrapperScreen(),
+        '/home': (_) => const HomeScreen(),
+        '/login': (_) => const LoginScreen(),
+        '/signup': (_) => const SignUpScreen(),
+      },
+    );
+  }
+}
